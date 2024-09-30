e6d883502e0960d61981ab09adc3b726a6f46a5c
Tobias Hölzer <tobiashoelzer@hotmail.com>
2024-09-30 02:35:10 +0200

    Fix versioning of mkdocs

--- darts-pipeline/app/run.py
+++ darts-pipeline/app/run.py
@@ -1,26 +1,44 @@
+"""Pipeline concept for rayify DARTS."""
+
 from dataclasses import dataclass
 from pathlib import Path
-from typing import Any
+from typing import Annotated, Any
 
 import numpy as np
 import ray
 import typer
 import xarray as xr
-from typing_extensions import Annotated
 
 app = typer.Typer()
 
 
 @dataclass
 class Tile:
+    """Xarray wrapper."""
+
     data: xr.Dataset
     path: str
 
     def __repr__(self):
+        """__repr__.
+
+        Returns:
+            str: Representation of the Tile object
+
+        """
         return f"Tile({self.path}, {self.data.dims})"
 
 
 def open_dataset_ray(row: dict[str, Any]) -> dict[str, Any]:
+    """open_dataset_ray.
+
+    Args:
+        row (dict[str, Any]): The row to process
+
+    Returns:
+        dict[str, Any]: The processed row
+
+    """
     data = xr.open_dataset(row["path"])
     tile = Tile(data, row["path"])
     return {
@@ -29,6 +47,15 @@ def open_dataset_ray(row: dict[str, Any]) -> dict[str, Any]:
 
 
 def batch_process(batch: dict[str, np.ndarray]) -> dict[str, np.ndarray]:
+    """batch_process.
+
+    Args:
+        batch (dict[str, np.ndarray]): Multiple rows combined in a batch.
+
+    Returns:
+        dict[str, np.ndarray]: The processed batch
+
+    """
     print(batch["tile"])
     # Here we could call our darts-* functions
     return batch
@@ -36,10 +63,18 @@ def batch_process(batch: dict[str, np.ndarray]) -> dict[str, np.ndarray]:
 
 @app.command()
 def run(
-    data: Annotated[Path, typer.Option(help="Path to the input data directory")] = Path(
-        "data"
-    ),
+    data: Annotated[Path, typer.Option(help="Path to the input data directory")] = Path("data"),
 ):
+    """Run the pipeline.
+
+    Args:
+        data (Path): Path to the data directory. Defaults to Path("data").
+
+    Raises:
+        FileNotFoundError: The data directory does not exist.
+        FileNotFoundError: No NetCDF files found in the data directory.
+
+    """
     ray.init()
 
     if not data.exists():
