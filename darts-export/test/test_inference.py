1e370ed4233e6f073f3b23ccb7f2401f83816f6a
Jonas Küpper <jonas.kuepper@awi.de>
2024-10-12 12:50:36 +0200

    make ruff even more happy

--- darts-export/test/test_inference.py
+++ darts-export/test/test_inference.py
@@ -1,7 +1,11 @@
 from pathlib import Path
+
+from xarray import Dataset
+
 from darts_export import inference
 
-def test_writeProbabilities(probabilities, tmp_path:Path):
+
+def test_writeProbabilities(probabilities: Dataset, tmp_path: Path):
     ds = inference.InferenceResultWriter(probabilities)
 
     ds.export_probabilities(tmp_path)
