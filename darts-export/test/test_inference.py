81ba89d2d02ed38bad2afc6faf7144827a2c3deb
Jonas Küpper <jonas.kuepper@awi.de>
2024-10-18 10:38:12 +0200

    export: rasterio vectorization/regionstats

--- darts-export/test/test_inference.py
+++ darts-export/test/test_inference.py
@@ -3,8 +3,9 @@ from pathlib import Path  # noqa: I001
 import geopandas as gpd
 import rasterio
 from darts_export import inference
-from osgeo import ogr
 from xarray import Dataset
+
+POLYGONOUTPUT_EXPECTED_COLUMNS = {"geometry", "Region_ID", "min", "max", "mean", "median", "std", "npixel"}
 
 
 def test_writeProbabilities(probabilities_1: Dataset, tmp_path: Path):
@@ -32,25 +33,48 @@ def test_writeBinarization(probabilities_1: Dataset, tmp_path: Path):
     assert rio_ds.dtypes[0] == "uint8"
 
 
-def test_writeVectors(probabilities_1: Dataset, tmp_path: Path):
-    gdal_has_parquet = ogr.GetDriverByName("Parquet") is not None
-
+def test_writeVectorsSimple(probabilities_1: Dataset, tmp_path: Path):
     ds = inference.InferenceResultWriter(probabilities_1)
-    ds.export_polygonized(tmp_path)
+    ds.export_polygonized(tmp_path, minimum_mapping_unit=0)
 
     assert (tmp_path / "pred_segments.gpkg").is_file()
-    if gdal_has_parquet:
-        assert (tmp_path / "pred_segments.parquet").is_file()
+    assert (tmp_path / "pred_segments.parquet").is_file()
 
     gpkg_layers = gpd.list_layers(tmp_path / "pred_segments.gpkg")
     assert len(gpkg_layers) == 1
     assert gpkg_layers.iloc[0]["name"] == "pred_segments"
 
-    gdf = gpd.read_file(tmp_path / "pred_segments.gpkg")
-    assert gdf.shape == (1, 2)  # one feature, two attributes (fid, DN)
-    assert gdf.iloc[0].geometry.wkt == (
+    expected_polygon = (
         "POLYGON (("
         "500007.5 4500003.5, 500007.5 4500004.5, 500006.5 4500004.5, 500006.5 4500008.5, 500007.5 4500008.5, "
         "500007.5 4500009.5, 500009.5 4500009.5, 500009.5 4500008.5, 500010.5 4500008.5, 500010.5 4500004.5, "
         "500009.5 4500004.5, 500009.5 4500003.5, 500007.5 4500003.5))"
     )
+
+    for suffix in ["gpkg", "parquet"]:
+        if suffix == "parquet":
+            gdf = gpd.read_parquet(tmp_path / f"pred_segments.{suffix}")
+        else:
+            gdf = gpd.read_file(tmp_path / f"pred_segments.{suffix}")
+        assert gdf.shape == (1, len(POLYGONOUTPUT_EXPECTED_COLUMNS))
+        assert gdf.iloc[0].geometry.wkt == expected_polygon
+        assert set(gdf.columns) == POLYGONOUTPUT_EXPECTED_COLUMNS
+
+
+def test_writeVectorsComplex(probabilities_2: Dataset, tmp_path: Path):
+    ds = inference.InferenceResultWriter(probabilities_2)
+    ds.export_polygonized(tmp_path)
+
+    assert (tmp_path / "pred_segments.gpkg").is_file()
+    assert (tmp_path / "pred_segments.parquet").is_file()
+
+    gpkg_layers = gpd.list_layers(tmp_path / "pred_segments.gpkg")
+    assert len(gpkg_layers) == 1
+    assert gpkg_layers.iloc[0]["name"] == "pred_segments"
+
+    for suffix in ["gpkg", "parquet"]:
+        if suffix == "parquet":
+            gdf = gpd.read_parquet(tmp_path / f"pred_segments.{suffix}")
+        else:
+            gdf = gpd.read_file(tmp_path / f"pred_segments.{suffix}")
+        assert gdf.shape == (4, len(POLYGONOUTPUT_EXPECTED_COLUMNS))
