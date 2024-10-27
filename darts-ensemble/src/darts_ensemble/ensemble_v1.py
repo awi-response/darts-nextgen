ea7934e782a6ed6305bb6e96b851f7146934dc06
Tobias Hölzer <tobiashoelzer@hotmail.com>
2024-10-27 17:11:13 +0100

    Make ensemble work

--- darts-ensemble/src/darts_ensemble/ensemble_v1.py
+++ darts-ensemble/src/darts_ensemble/ensemble_v1.py
@@ -13,38 +13,141 @@ class EnsembleV1:
         self,
         rts_v6_tcvis_model_path: str | Path,
         rts_v6_notcvis_model_path: str | Path,
-        binarize_threshold: float = 0.5,
     ):
         """Initialize the ensemble.
 
         Args:
             rts_v6_tcvis_model_path (str | Path): Path to the model trained with TCVIS data.
             rts_v6_notcvis_model_path (str | Path): Path to the model trained without TCVIS data.
-            binarize_threshold (float, optional): Threshold to binarize the ensemble output. Defaults to 0.5.
 
         """
         self.rts_v6_tcvis_model = SMPSegmenter(rts_v6_tcvis_model_path)
         self.rts_v6_notcvis_model = SMPSegmenter(rts_v6_notcvis_model_path)
-        self.threshold = binarize_threshold
 
-    def __call__(self, tile: xr.Dataset, keep_inputs: bool = False) -> xr.Dataset:
+    def segment_tile(
+        self,
+        tile: xr.Dataset,
+        patch_size: int = 1024,
+        overlap: int = 16,
+        batch_size: int = 8,
+        reflection: int = 0,
+        keep_inputs: bool = False,
+    ) -> xr.Dataset:
+        """Run inference on a tile.
+
+        Args:
+            tile: The input tile, containing preprocessed, harmonized data.
+            patch_size (int): The size of the patches. Defaults to 1024.
+            overlap (int): The size of the overlap. Defaults to 16.
+            batch_size (int): The batch size for the prediction, NOT the batch_size of input tiles.
+            Tensor will be sliced into patches and these again will be infered in batches. Defaults to 8.
+            reflection (int): Reflection-Padding which will be applied to the edges of the tensor. Defaults to 0.
+            keep_inputs (bool, optional): Whether to keep the input probabilities in the output. Defaults to False.
+
+        Returns:
+            Input tile augmented by a predicted `probabilities` layer with type float32 and range [0, 1].
+
+        """
+        tcvis_probabilities = self.rts_v6_tcvis_model.segment_tile(
+            tile, patch_size=patch_size, overlap=overlap, batch_size=batch_size, reflection=reflection
+        )["probabilities"].copy()
+        notcvis_propabilities = self.rts_v6_notcvis_model.segment_tile(
+            tile, patch_size=patch_size, overlap=overlap, batch_size=batch_size, reflection=reflection
+        )["probabilities"].copy()
+
+        tile["probabilities"] = (tcvis_probabilities + notcvis_propabilities) / 2
+
+        if keep_inputs:
+            tile["probabilities-tcvis"] = tcvis_probabilities
+            tile["probabilities-notcvis"] = notcvis_propabilities
+
+        return tile
+
+    def segment_tile_batched(
+        self,
+        tiles: list[xr.Dataset],
+        patch_size: int = 1024,
+        overlap: int = 16,
+        batch_size: int = 8,
+        reflection: int = 0,
+        keep_inputs: bool = False,
+    ) -> list[xr.Dataset]:
+        """Run inference on a list of tiles.
+
+        Args:
+            tiles: The input tiles, containing preprocessed, harmonized data.
+            patch_size (int): The size of the patches. Defaults to 1024.
+            overlap (int): The size of the overlap. Defaults to 16.
+            batch_size (int): The batch size for the prediction, NOT the batch_size of input tiles.
+            Tensor will be sliced into patches and these again will be infered in batches. Defaults to 8.
+            reflection (int): Reflection-Padding which will be applied to the edges of the tensor. Defaults to 0.
+            keep_inputs (bool, optional): Whether to keep the input probabilities in the output. Defaults to False.
+
+        Returns:
+            A list of input tiles augmented by a predicted `probabilities` layer with type float32 and range [0, 1].
+
+        """
+        for tile in tiles:  # Note that tile is still a reference -> tiles will be changed!
+            tcvis_probabilities = self.rts_v6_tcvis_model.segment_tile(
+                tile, patch_size=patch_size, overlap=overlap, batch_size=batch_size, reflection=reflection
+            )["probabilities"].copy()
+            notcvis_propabilities = self.rts_v6_notcvis_model.segment_tile(
+                tile, patch_size=patch_size, overlap=overlap, batch_size=batch_size, reflection=reflection
+            )["probabilities"].copy()
+
+            tile["probabilities"] = (tcvis_probabilities + notcvis_propabilities) / 2
+
+            if keep_inputs:
+                tile["probabilities-tcvis"] = tcvis_probabilities
+                tile["probabilities-notcvis"] = notcvis_propabilities
+
+        return tiles
+
+    def __call__(
+        self,
+        input: xr.Dataset | list[xr.Dataset],
+        patch_size: int = 1024,
+        overlap: int = 16,
+        batch_size: int = 8,
+        reflection: int = 0,
+        keep_inputs: bool = False,
+    ) -> xr.Dataset:
         """Run the ensemble on the given tile.
 
         Args:
+            input (xr.Dataset | list[xr.Dataset]): A single tile or a list of tiles.
             tile (xr.Dataset): Input tile from preprocessing.
+            patch_size (int): The size of the patches. Defaults to 1024.
+            overlap (int): The size of the overlap. Defaults to 16.
+            batch_size (int): The batch size for the prediction, NOT the batch_size of input tiles.
+            Tensor will be sliced into patches and these again will be infered in batches. Defaults to 8.
+            reflection (int): Reflection-Padding which will be applied to the edges of the tensor. Defaults to 0.
             keep_inputs (bool, optional): Whether to keep the input probabilities in the output. Defaults to False.
 
         Returns:
             xr.Dataset: Output tile with the ensemble applied.
 
+        Raises:
+            ValueError: in case the input is not an xr.Dataset or a list of xr.Dataset
+
         """
-        tcvis_tile = self.rts_v6_tcvis_model.segment_tile(tile)
-        notcvis_tile = self.rts_v6_notcvis_model.segment_tile(tile)
-
-        tile["probabilities"] = (tcvis_tile["probabilities"] + notcvis_tile["probabilities"]) / 2
-
-        if keep_inputs:
-            tile["probabilities-tcvis"] = tcvis_tile["probabilities"]
-            tile["probabilities-notcvis"] = notcvis_tile["probabilities"]
-
-        return tile
+        if isinstance(input, xr.Dataset):
+            return self.segment_tile(
+                input,
+                patch_size=patch_size,
+                overlap=overlap,
+                batch_size=batch_size,
+                reflection=reflection,
+                keep_inputs=keep_inputs,
+            )
+        elif isinstance(input, list):
+            return self.segment_tile_batched(
+                input,
+                patch_size=patch_size,
+                overlap=overlap,
+                batch_size=batch_size,
+                reflection=reflection,
+                keep_inputs=keep_inputs,
+            )
+        else:
+            raise ValueError("Input must be an xr.Dataset or a list of xr.Dataset.")
