76ddf1f878ceb41cb65b8e559f9204c33ae88157
tcnichol <tcnichol@illinois.edu>
2024-12-02 14:53:40 -0600

    full paths for directories in run_native

--- darts/src/darts/run_native.py
+++ darts/src/darts/run_native.py
@@ -1,13 +1,13 @@
 from darts.native import run_native_planet_pipeline
 from pathlib import Path
-orthotiles_dir = "data/input/planet/PSOrthoTile"
-scenes_dir = "data/input/planet/PSScene"
-arcticdem_slope_vrt = "data/input/ArcticDEM/slope.vrt"
-arcticdem_elevation_vrt = "data/input/ArcticDEM/elevation.vrt"
-output_data_dir = "data/output"
-model_dir = "models"
+orthotiles_dir = "/home/toddn/pdg/darts-nextgen/rts_dtaset01/input/planet/PSOrthoTile"
+scenes_dir = "/home/toddn/pdg/darts-nextgen/rts_dtaset01/input/planet/PSScene"
+arcticdem_slope_vrt = "/home/toddn/pdg/darts-nextgen/rts_dtaset01/input/ArcticDEM/slope.vrt"
+arcticdem_elevation_vrt = "/home/toddn/pdg/darts-nextgen/rts_dtaset01/input/ArcticDEM/elevation.vrt"
+output_data_dir = "/home/toddn/pdg/darts-nextgen/data/output"
+model_dir = "/home/toddn/pdg/darts-nextgen/rts_dtaset01/models"
 
-tcvis_dir = "tcvis_dir"
+tcvis_dir = "/home/toddn/pdg/darts-nextgen/tcvis_dir"
 
 if __name__ == "__main__":
     print("Running the pipeline")
