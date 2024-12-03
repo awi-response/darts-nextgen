d5bc3173b8b2740d064eb99fd19824aa6f53b985
tcnichol <tcnichol@illinois.edu>
2024-12-03 17:30:00 -0600

    fixes error, not sure if this is a good policy, but works enough

--- darts-preprocessing/src/darts_preprocessing/preprocess.py
+++ darts-preprocessing/src/darts_preprocessing/preprocess.py
@@ -31,6 +31,7 @@ def preprocess_legacy(
     ds_arcticdem: xr.Dataset,
     ds_tcvis: xr.Dataset,
     ds_data_masks: xr.Dataset,
+    compat_overridee: bool = True,
 ) -> xr.Dataset:
     """Preprocess optical data with legacy (DARTS v1) preprocessing steps.
 
@@ -58,8 +59,10 @@ def preprocess_legacy(
     # we dont need to calculate them here
 
     # merge to final dataset
-    ds_merged = xr.merge([ds_optical, ds_ndvi, ds_arcticdem, ds_tcvis, ds_data_masks])
-
+    if compat_overridee:
+        ds_merged = xr.merge([ds_optical, ds_ndvi, ds_arcticdem, ds_tcvis, ds_data_masks], compat='override')
+    else:
+        ds_merged = xr.merge([ds_optical, ds_ndvi, ds_arcticdem, ds_tcvis, ds_data_masks])
     return ds_merged
 
 
