e8d63bb9c88fe6ca74cc94df45f2fa8b047aa8d8
tcnichol <tcnichol@illinois.edu>
2025-01-01 15:00:01 -0600

    setup for running fast pipeline

--- darts/src/darts/run_native.py
+++ darts/src/darts/run_native.py
@@ -1,4 +1,4 @@
-from darts.native import run_native_planet_pipeline
+from darts.native import run_native_planet_pipeline, run_native_planet_pipeline_fast
 from pathlib import Path
 orthotiles_dir = "/home/toddn/pdg/darts-nextgen/rts_dtaset01/input/planet/PSOrthoTile"
 scenes_dir = "/home/toddn/pdg/darts-nextgen/rts_dtaset01/input/planet/PSScene"
@@ -6,11 +6,20 @@ arcticdem_slope_vrt = "/home/toddn/pdg/darts-nextgen/rts_dtaset01/input/ArcticDE
 arcticdem_elevation_vrt = "/home/toddn/pdg/darts-nextgen/rts_dtaset01/input/ArcticDEM/elevation.vrt"
 output_data_dir = "/home/toddn/pdg/darts-nextgen/data/output"
 model_dir = "/home/toddn/pdg/darts-nextgen/rts_dtaset01/models"
+arctic_dem_dir = "/home/toddn/pdg/darts-nextgen/rts_dtaset01/input/ArcticDEM"
 
 tcvis_dir = "/home/toddn/pdg/darts-nextgen/tcvis_dir"
 
 if __name__ == "__main__":
     print("Running the pipeline")
+    run_native_planet_pipeline_fast(orthotiles_dir=Path(orthotiles_dir),
+                                scenes_dir=Path(scenes_dir),
+                                output_data_dir=Path(output_data_dir),
+                                arcticdem_dir=Path(arctic_dem_dir),
+                                model_dir=Path(model_dir),
+                                tcvis_dir=Path(tcvis_dir),
+                                ee_project='pdg-project-406720')
+
     run_native_planet_pipeline(orthotiles_dir=Path(orthotiles_dir),
                                scenes_dir=Path(scenes_dir),
                                output_data_dir=Path(output_data_dir),
