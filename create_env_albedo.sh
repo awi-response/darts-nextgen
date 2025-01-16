714befc042678ed4ff80d858f69fd794289ab860
tcnichol <tcnichol@illinois.edu>
2025-01-16 15:50:37 -0600

    adding TODO

--- create_env_albedo.sh
+++ create_env_albedo.sh
@@ -9,37 +9,30 @@ echo "${CONDA_ENVS}"
 conda env create -f environment.yml
 
 conda activate darts_albedo
-#
-#conda create -n darts-nextgen python=3.11 -y
-#
-#conda activate darts-nextgen
-#
+
+
 CUR_PYTHON="$(which python)"
-#
-# pip install '.[dev]'
-#
-#
+
+# TODO this has to go last for darst cli to work!!!
+pip install '.[dev]'
+
+
 cd ${current_directory}/darts-acquisition
 new_dir=$(pwd)
 echo "${new_dir}"
 pip install '.[dev]'
 
-
 cd ${current_directory}/darts-ensemble
 pip install '.[dev]'
-
 
 cd ${current_directory}/darts-export
 pip install '.[dev]'
 
-
 cd ${current_directory}/darts-postprocessing
 pip install '.[dev]'
 
-
 cd ${current_directory}/darts-preprocessing
 pip install '.[dev]'
-
 
 cd ${current_directory}/darts-segmentation
 pip install '.[dev]'
@@ -51,7 +44,4 @@ pip install '.[dev]'
 
 cd ${current_directory}/darts-utils
 pip install '.[dev]'
-
-cd ${current_directory}
-
 echo Created Environment
