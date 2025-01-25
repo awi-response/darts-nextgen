29b15f5d96748f08e51077ac7f46f182e9372c40
tcnichol <tcnichol@illinois.edu>
2025-01-25 16:09:52 -0600

    these are nohup tests and will be removed later. I am adding these since running scripts have resulted in some zombie processes. I want to test using timeout with processes running in nohup as part of finding a solution for avoiding this problem in the future

---
+++ test/nohup_test_write.py
@@ -0,0 +1,19 @@
+import time
+import os
+
+path_to_file = '/taiga/toddn/test/nohup_test.txt'
+
+for i in range(0, 10):
+    print(f"We are doing round {i} and will rest for one minute.")
+    if os.path.exists(path_to_file):
+        print(f"The file already exists")
+        with open(path_to_file, 'a+') as f:
+            current_line = "We did this for minute " + str(i) + '\n'
+            f.write(current_line)
+    else:
+        print(f"The file does not already exist")
+        with open(path_to_file, 'w+') as f:
+            current_line = "We did this for minute " + str(i) + '\n'
+            f.write(current_line)
+    time.sleep(60)
+print(f"We finished the whole thing")
