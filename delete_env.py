328e2ba354a9155580a1928d2462ad727ffbd977
tcnichol <tcnichol@illinois.edu>
2025-01-29 11:49:08 -0600

    fix environments

---
+++ delete_env.py
@@ -0,0 +1,10 @@
+import os
+
+top = '/taiga/toddn/conda/envs/darts_albedo_2'
+
+import os
+for root, dirs, files in os.walk(top, topdown=False):
+    for name in files:
+        os.remove(os.path.join(root, name))
+    for name in dirs:
+        os.rmdir(os.path.join(root, name))
