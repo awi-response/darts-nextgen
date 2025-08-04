f3406a5e76b6904d635998e89178d4e55de21e78
Tobias Hölzer <tobiashoelzer@hotmail.com>
2025-08-04 18:42:00 +0200

    Add Vector-Ruggedness-Measure, Dissection Index and Terrain-Ruggedness-Index to arcticdem preprocessing

--- darts-preprocessing/src/darts_preprocessing/engineering/arcticdem.py
+++ darts-preprocessing/src/darts_preprocessing/engineering/arcticdem.py
@@ -1,18 +1,71 @@
 """Computation of ArcticDEM derived products."""
 
 import logging
+from dataclasses import dataclass
 from math import ceil
 
+import numpy as np
 import xarray as xr
 from stopuhr import stopwatch
 from xrspatial import aspect, convolution, curvature, hillshade, slope
 from xrspatial.utils import has_cuda_and_cupy
+
+from darts_preprocessing.engineering.dissection_index import dissection_index
 
 logger = logging.getLogger(__name__.replace("darts_", "darts."))
 
 if has_cuda_and_cupy():
     import cupy as cp  # type: ignore
     import cupy_xarray  # noqa: F401 # type: ignore
+
+
+@dataclass(frozen=True)
+class Distance:
+    """Convenience class to represent a distance in pixels and meters."""
+
+    pixel: int
+    meter: float
+
+    def __repr__(self):  # noqa: D105
+        return f"{self.pixel}px ({self.meter}m)"
+
+    @classmethod
+    def parse(cls, v: int | float | str, res: float) -> "Distance":
+        """Parse a distance from a string or numeric value.
+
+        If the input is a string, it can be in the format of "10px" or "10m".
+        If it is a numeric value, it is interpreted as meters and converted to pixels based on the resolution.
+
+        Args:
+            v (int | float | str): The input distance value.
+            res (float): The resolution in meters per pixel.
+
+        Raises:
+            ValueError: If the input distance is negative.
+            ValueError: If the input distance is not a valid string format.
+            TypeError: If the input distance is not a string, int, or float.
+
+        Returns:
+            Distance: The parsed distance in pixels and meters.
+
+        """
+        if isinstance(v, str):
+            if v.endswith("px"):
+                pixel = int(v[:-2])
+                meter = pixel * res
+            elif v.endswith("m"):
+                meter = float(v[:-1])
+                pixel = ceil(meter / res)
+            else:
+                raise ValueError(f"Invalid distance format: {v}")
+        elif isinstance(v, (int, float)):
+            if v < 0:
+                raise ValueError("Distance cannot be negative")
+            pixel = ceil(v / res)
+            meter = pixel * res
+        else:
+            raise TypeError(f"Invalid type for distance: {type(v)}")
+        return cls(pixel=pixel, meter=meter)
 
 
 @stopwatch.f("Calculating TPI", printer=logger.debug, print_kwargs=["outer_radius", "inner_radius"])
@@ -30,28 +83,20 @@ def calculate_topographic_position_index(arcticdem_ds: xr.Dataset, outer_radius:
     """
     cellsize_x, cellsize_y = convolution.calc_cellsize(arcticdem_ds.dem)  # Should be equal to the resolution of the DEM
     # Use an annulus kernel if inner_radius is greater than 0
-    outer_radius_m = f"{outer_radius}m"
-    outer_radius_px = f"{ceil(outer_radius / cellsize_x)}px"
+    outer_radius: Distance = Distance.parse(outer_radius, cellsize_x)
     if inner_radius > 0:
-        inner_radius_m = f"{inner_radius}m"
-        inner_radius_px = f"{ceil(inner_radius / cellsize_x)}px"
-        kernel = convolution.annulus_kernel(cellsize_x, cellsize_y, outer_radius_m, inner_radius_m)
+        inner_radius: Distance = Distance.parse(inner_radius, cellsize_x)
+        kernel = convolution.annulus_kernel(cellsize_x, cellsize_y, outer_radius.meter, inner_radius.meter)
         attr_cell_description = (
-            f"within a ring at a distance of {inner_radius_px}-{outer_radius_px} cells "
-            f"({inner_radius_m}-{outer_radius_m}) away from the focal cell."
+            f"within a ring at a distance of {inner_radius}-{outer_radius} cells away from the focal cell."
         )
         logger.debug(
-            f"Calculating Topographic Position Index with annulus kernel of "
-            f"{inner_radius_px}-{outer_radius_px} ({inner_radius_m}-{outer_radius_m}) cells."
+            f"Calculating Topographic Position Index with annulus kernel of {inner_radius}-{outer_radius} cells."
         )
     else:
-        kernel = convolution.circle_kernel(cellsize_x, cellsize_y, outer_radius_m)
-        attr_cell_description = (
-            f"within a circle at a distance of {outer_radius_px} cells ({outer_radius_m}) away from the focal cell."
-        )
-        logger.debug(
-            f"Calculating Topographic Position Index with circle kernel of {outer_radius_px} ({outer_radius_m}) cells."
-        )
+        kernel = convolution.circle_kernel(cellsize_x, cellsize_y, outer_radius.meter)
+        attr_cell_description = f"within a circle at a distance of {outer_radius} cells away from the focal cell."
+        logger.debug(f"Calculating Topographic Position Index with circle kernel of {outer_radius} cells.")
 
     if has_cuda_and_cupy() and arcticdem_ds.cupy.is_cupy:
         kernel = cp.asarray(kernel)
@@ -162,4 +207,160 @@ def calculate_curvature(arcticdem_ds: xr.Dataset) -> xr.Dataset:
         "_FillValue": float("nan"),
     }
     arcticdem_ds["curvature"] = curvature_da.compute()
+    return arcticdem_ds
+
+
+@stopwatch("Calculating TRI", printer=logger.debug)
+def calculate_terrain_ruggedness_index(arcticdem_ds: xr.Dataset, neighborhood_size: int) -> xr.Dataset:
+    """Calculate the Terrain Ruggedness Index (TRI) from an ArcticDEM Dataset.
+
+    Definition from ESRI:
+    TRI expresses the amount of elevation difference between adjacent cells of a DEM.
+    Using methodology developed by Riley et al (1999) and published in the paper
+    “A Terrain ruggedness Index That Quantifies Topographic heterogeneity”,
+    the tool measures the difference in elevation values from a center cell and eight cells directly surrounding it.
+    Then, the eight elevation differences are squared and averaged.
+    The square root of this average results is a TRI measurement for the center cell.
+    This calculation is then conducted on every cell of the DEM.
+
+    Args:
+        arcticdem_ds (xr.Dataset): The ArcticDEM Dataset containing the 'dem' variable.
+        neighborhood_size (int): The neighborhood_size in meters for the TRI calculation.
+
+    Returns:
+        xr.Dataset: The input Dataset with the calculated TRI added as a new variable 'tri'.
+
+    """
+    cellsize_x, _cellsize_y = convolution.calc_cellsize(arcticdem_ds.dem)
+
+    neighborhood_size: Distance = Distance.parse(neighborhood_size, cellsize_x)
+
+    kernel = np.ones((neighborhood_size.pixel, neighborhood_size.pixel), dtype=float)
+    kernel[neighborhood_size.pixel // 2, neighborhood_size.pixel // 2] = 0  # Set the center cell to 0
+    kernel = convolution.custom_kernel(kernel)
+    logger.debug(f"Calculating Terrain Ruggedness Index with square kernel of radius {neighborhood_size} cells.")
+
+    if has_cuda_and_cupy() and arcticdem_ds.cupy.is_cupy:
+        kernel = cp.asarray(kernel)
+
+    # Kernel compute of TRI as described here:
+    # https://sites.utexas.edu/utarima/files/2024/02/terrain_roughness_index.pdf
+    dem_squared = arcticdem_ds.dem**2
+    focal_sum = convolution.convolution_2d(arcticdem_ds.dem, kernel)
+    focal_sum_squared = convolution.convolution_2d(dem_squared, kernel)
+    tri = np.sqrt((kernel.size - 1) * dem_squared - 2 * arcticdem_ds.dem * focal_sum + focal_sum_squared)
+
+    tri.attrs = {
+        "long_name": "Terrain Ruggedness Index",
+        "units": "m",
+        "description": (
+            "The difference between the elevation of a cell and the mean elevation of the surrounding"
+            f" cells within a square kernel of radius {neighborhood_size} cells."
+        ),
+        "source": "ArcticDEM",
+        "_FillValue": float("nan"),
+    }
+
+    arcticdem_ds["tri"] = tri.compute()
+    return arcticdem_ds
+
+
+@stopwatch("Calculating Vector Ruggedness Measure", printer=logger.debug)
+def calculate_vector_ruggedness_measure(arcticdem_ds: xr.Dataset, neighborhood_size: int) -> xr.Dataset:
+    """Calculate the Vector Ruggedness Measure (VRM) from an ArcticDEM Dataset.
+
+    Implementation of the vector ruggedness measure described in Sappington, J.M.,
+    K.M. Longshore, and D.B. Thomson. 2007. Quantifying Landscape Ruggedness for
+    Animal Habitat Analysis: A case Study Using Bighorn Sheep in the Mojave Desert.
+    Journal of Wildlife Management. 71(5): 1419-1426.
+
+    Args:
+        arcticdem_ds (xr.Dataset): The ArcticDEM Dataset containing the 'dem' variable.
+        neighborhood_size (int): The size of the neighborhood window (in meters) for the calculation.
+
+    Returns:
+        xr.Dataset: The input Dataset with the calculated VRM added as a new variable 'vrm'.
+
+    """
+    # Calculate slope and aspect
+    slope_rad = arcticdem_ds.slope * (np.pi / 180)  # Convert to radians
+    aspect_rad = arcticdem_ds.aspect * (np.pi / 180)  # Convert to radians
+
+    # Calculate x, y, and z components of unit vectors
+    xy = np.sin(slope_rad)
+    z = np.cos(slope_rad)
+
+    # Handle flat areas (where aspect = -1)
+    if has_cuda_and_cupy() and arcticdem_ds.cupy.is_cupy:
+        aspect_rad.variable._data = cp.where(aspect_rad.variable._data == -1, 0, aspect_rad.variable._data)
+        # aspect_rad = aspect_rad.copy(data=aspect_rad_raw)
+    else:
+        aspect_rad = xr.where(aspect_rad == -1, 0, aspect_rad)
+    x = np.sin(aspect_rad) * xy
+    y = np.cos(aspect_rad) * xy
+
+    # Get neighborhood_size in pixels
+    neighborhood_size: Distance = Distance.parse(neighborhood_size, arcticdem_ds.odc.geobox.resolution.x)
+    # Create convolution kernel for focal sum
+    kernel = np.ones((neighborhood_size.pixel, neighborhood_size.pixel), dtype=float) / neighborhood_size.pixel**2
+    kernel = convolution.custom_kernel(kernel)
+
+    if has_cuda_and_cupy() and arcticdem_ds.cupy.is_cupy:
+        kernel = cp.asarray(kernel)
+
+    logger.debug(f"Calculating Vector Ruggedness Measure with square kernel of size {neighborhood_size} cells.")
+
+    # TODO: Write a custom kernel for this for speedup and smaller memory footprint
+    # Calculate sums of x, y, and z components in the neighborhood
+    x_sum = convolution.convolution_2d(x, kernel)
+    y_sum = convolution.convolution_2d(y, kernel)
+    z_sum = convolution.convolution_2d(z, kernel)
+
+    # Calculate the resultant vector magnitude
+    vrm = 1 - np.sqrt(x_sum**2 + y_sum**2 + z_sum**2)
+
+    vrm.attrs = {
+        "long_name": "Vector Ruggedness Measure",
+        "units": "",
+        "description": (
+            f"Vector ruggedness measure calculated using a {neighborhood_size} neighborhood. "
+            "Values range from 0 (smooth) to 1 (most rugged)."
+        ),
+        "source": "ArcticDEM",
+        "_FillValue": float("nan"),
+    }
+
+    arcticdem_ds["vrm"] = vrm.compute()
+    return arcticdem_ds
+
+
+@stopwatch("Calculating Dissection Index", printer=logger.debug)
+def calculate_dissection_index(arcticdem_ds: xr.Dataset, neighborhood_size: int) -> xr.Dataset:
+    """Calculate the Dissection Index (DI) from an ArcticDEM Dataset.
+
+    Args:
+        arcticdem_ds (xr.Dataset): The ArcticDEM Dataset containing the 'dem' variable.
+        neighborhood_size (int): The size of the neighborhood window (in meters) for the calculation.
+
+    Returns:
+        xr.Dataset: The input Dataset with the calculated DI added as a new variable 'di'.
+
+    """
+    # Get neighborhood_size in pixels
+    neighborhood_size: Distance = Distance.parse(neighborhood_size, arcticdem_ds.odc.geobox.resolution.x)
+
+    di = dissection_index(arcticdem_ds.dem, window_size=neighborhood_size.pixel)
+
+    di.attrs = {
+        "long_name": "Dissection Index",
+        "units": "",
+        "description": (
+            f"Dissection index calculated using a {neighborhood_size} neighborhood. "
+            "Values range from 0 (smooth) to 1 (most rugged)."
+        ),
+        "source": "ArcticDEM",
+        "_FillValue": float("nan"),
+    }
+
+    arcticdem_ds["di"] = di.compute()
     return arcticdem_ds
